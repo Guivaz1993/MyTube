edaa705746d8a294be9396c12e5e3bf65d6491b3
Guilherme <guivaz1993@gmail.com>
2022-10-27 23:48:07 -0300

    deadline

--- frontend/src/components/ModalNewRoom/Index.jsx
+++ frontend/src/components/ModalNewRoom/Index.jsx
@@ -1,17 +1,37 @@
+/* eslint-disable comma-dangle */
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import useUser from "../../hooks/useUser";
+import { post } from "../../services/functions";
+import { getItem } from "../../utils/storage";
 
 import "./styles.css";
 
 export default function Modal() {
   const { openModalRoom, toggleModalRoom } = useUser();
   const navigate = useNavigate();
+  const token = getItem("token");
 
-  function LoginOnClick() {
-    toggleModalRoom();
-    navigate("/room");
+  async function newRoomOnClick() {
+    try {
+      const { data, status } = await post(
+        "/newroom",
+        {
+          name: "nova Sala",
+          video_id: 1,
+        },
+        token
+      );
+      if (status !== 201) {
+        return toast.error(data.message);
+      }
+
+      return navigate(`/room/${data.id}`);
+    } catch (error) {
+      return toast.error(error.message);
+    }
   }
   useEffect(() => {}, []);
   return (
@@ -28,8 +48,11 @@ export default function Modal() {
           openModalRoom ? "OpenModalRoom ModalRoom" : "CloseModalRoom ModalRoom"
         }
       >
-        <button type="button" onClick={() => LoginOnClick()}>
+        <button type="button" onClick={() => newRoomOnClick()}>
           Nova Sala
+        </button>
+        <button type="button" onClick={() => toggleModalRoom()}>
+          Fechar
         </button>
       </div>
     </>
